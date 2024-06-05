dbe1cdab43c858bb50f71b3c92531fd2c51bce93
TImothy Akanbi <knbtimothy@gmail.com>
2024-06-05 12:13:03 +0100

    Fixed problem

--- ecommerce-website/src/app/(auth)/reset-password/page.tsx
+++ ecommerce-website/src/app/(auth)/reset-password/page.tsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { Suspense } from "react"
-import Form from "./Form"
-
-
-const page = () => {
-  return (
-    <section className='text-center mt-[30px]'>
-      <h1 className='text-[#AF1328] text-[40px] font-sevenHundred mb-[19px]'>Reset your Password.</h1>
-      <h5 className='text-[#B3B1B1] text-sixteen font-fourHundrend mb-[54px]'>Reset your password to continue.</h5>
-      <Suspense fallback={<></>}>
-        <Form />
-      </Suspense>
-    </section>
-  )
-}
-
-=======
 import Form from "./Form"
 
 
@@ -29,5 +11,4 @@ const page = () => {
   )
 }
 
->>>>>>> fa096c5419a1cc3487731f6ea6c5b651a34dad8a
 export default page
--- ecommerce-website/src/app/(auth)/verify/page.tsx
+++ ecommerce-website/src/app/(auth)/verify/page.tsx
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-import { Metadata } from "next";
-import VerificationButton from "./VerificationButton";
-import { Suspense } from "react";
 
-export const metadata: Metadata = {
-    title: "MARCSONIC | Verify your email",
-    description: "Marcsonic- Home of shopping",
-  };
-
-const page = () => {
-  return (
-    <main className="text-center mt-[120px]">
-        <h1 className="text-[#AF1328] font-sevenHundred text-[40px] mb-[47px]">Verify your email to finish signing up to marcsonic</h1>
-        <h5 className="text-[#574240] font-[700] text-sixteen mb-[20px]">Thank you for choosing Kwek</h5>
-        <h5 className="text-[#574240] font-[700] text-sixteen">Please confirm that <span className="text-[#574240]">godsonakinbayo@gmail.com</span> is your email address by clicking the </h5>
-        <h5 className="text-[#574240] font-[700] text-sixteen mb-[72px]">button below within the next 24hours</h5>
-        <Suspense fallback={<></>}>
-          <VerificationButton />
-        </Suspense>
-    </main>
-  )
-}
-
-=======
 import { Metadata } from "next";
 import VerificationButton from "./VerificationButton";
 
@@ -43,5 +19,4 @@ const page = () => {
   )
 }
 
->>>>>>> fa096c5419a1cc3487731f6ea6c5b651a34dad8a
 export default page
