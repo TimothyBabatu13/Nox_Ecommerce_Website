94b27ae36b0cadd036830ac18325ff11411e2b40
TImothy Akanbi <knbtimothy@gmail.com>
2024-06-05 12:16:55 +0100

    Fixed problem

--- ecommerce-website/src/components/Cart.tsx
+++ ecommerce-website/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import LinkImage from './LinkImage'
 import { CartIcon } from './SVGs';
@@ -23,30 +23,5 @@ const Cart = () => {
   )
 }
 
-=======
-import React from 'react'
-import LinkImage from './LinkImage'
-import { CartIcon } from './SVGs';
 
-const Cart = () => {
-  const number = 2;
-  return (
-    <div className='hidden md:block relative'>
-        <LinkImage
-          href="/home/cart"    
-          text="Cart"
-        >
-          <CartIcon />
-        </LinkImage>
-        <div 
-          className='absolute top-[-3px] left-[35px] w-[12px] h-[12px] text-[8px] font-[500] flex text-[#333333] items-center justify-center rounded-[50px] bg-[#FFC107] border border-primary'
-        >
-          {number}
-        </div>
-        
-    </div>
-  )
-}
-
->>>>>>> fa096c5419a1cc3487731f6ea6c5b651a34dad8a
 export default Cart
